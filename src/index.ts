92bd633551b7dfdfd25949208b4b67f535c30a28
Kay <kehiiiiya@gmail.com>
2024-07-06 09:37:08 +0000

    feat: get command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
-console.log("Hello, World!");
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+    .name("tonion")
+    .description("tonion is a TON and Tact development tool and a tonion library package manager.")
+    .version("0.0.1");
+
+program.command("get <package>")
+    .action(() => {
+    })
+    .description("Get a Tact package from the Tonion contracts repository.");
+
+program.parse(process.argv);
